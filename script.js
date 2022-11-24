a84716ad78582e55845f9ad6e45f95c527da8d6d
AbogeJr <junioraboge@gmail.com>
2022-11-24 14:25:28 +0300

    update

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 const menu_btn = document.getElementById("menu-btn");
 const menu = document.getElementById("menu");
+let show = false;
 
 menu_btn.addEventListener(
   "click",
   () => {
+    show = !show;
     menu.classList.toggle("show");
+    show
+      ? (menu_btn.innerHTML = '<span class="close">&times;</span>')
+      : (menu_btn.innerHTML = '<img src="./menu.png" />');
   },
   false
 );
